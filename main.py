ead6bf73d9c6e4930a86c3c2bcfe30426d484eaa
sergey-png <svsobolev@miem.hse.ru>
2022-03-26 18:37:30 +0300

    start oop structure added first main method to autoscale

--- main.py
+++ main.py
@@ -3,32 +3,52 @@ from time import sleep
 import numpy as np
 
 visa.log_to_screen()
-rm = visa.ResourceManager()
-# print(rm.list_resources())
-rigol_device_id = rm.list_resources()[0]
-
+'''
 #  osc.read osc.write osc.query
 #  You can use visa-shell
 #  To run visa-shell insert command: pyvisa-shell
 #  After opening device You can talk to the device using "write", "read" or "query".
 #  The default end of message is added to each message.
 #  osc.query - method 621 string in pyvisa/resources/messagebased.py
-osc = rm.open_resource(rigol_device_id)  # open device to work with
-print(f'QUERY returned answer = {osc.query("*IDN?")}')  # Command to check ID of opened device
-print(f"Opened res = {rm.list_opened_resources()}")
+device = rm.open_resource(rigol_device_id)  # open device to work with
+print(f'QUERY returned answer = {device.query("*IDN?")}')  # Command to check ID of opened device
+'''
 
 
-def autoscale(oscillator):
-    """
-    Enable the waveform auto setting function. The oscilloscope will automatically adjust the
-    vertical scale, horizontal timebase, and trigger mode according to the input signal to
-    realize optimum waveform display. This command is equivalent to pressing the AUTO key
-    at the front panel.
-    :param oscillator: opened device to connect
-    """
-    command = ":AUToscale"
-    oscillator.write(command)
-    return 0
+class RigolAPI:
+    def __init__(self):
+        self.rm = visa.ResourceManager()
+        if len(self.rm.list_resources()) > 0:
+            self.rigol_device_id = self.rm.list_resources()[0]
+            self.device = self.rm.open_resource(self.rigol_device_id)  # Now can self.device. -/read, -/write, -/query
+            self.device.timeout = 13000
+        else:
+            self.rigol_device_id = None
+
+    def device_id(self):
+        return self.rigol_device_id
+
+    def resources_list(self):
+        return self.rm.list_resources()
+
+    def opened_resources(self):
+        return self.rm.list_opened_resources()
+
+    # ---------- MAIN METHODS -----------
+
+    def autoscale_func(self):
+        """
+        Enable the waveform auto setting function. The oscilloscope will automatically adjust the
+        vertical scale, horizontal timebase, and trigger mode according to the input signal to
+        realize optimum waveform display. This command is equivalent to pressing the AUTO key
+        at the front panel.
+        :param oscillator: opened device to connect
+        """
+        self.device.write(":AUToscale")
+        return 0
 
 
-print(f"Write to osc = {autoscale(osc)}")
+if __name__ == "__main__":
+    rigol = RigolAPI()
+    rigol.autoscale_func()
+    print("------------END------------")
