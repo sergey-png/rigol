cb30a3e5a50adde1d7a117e014c2b12d5f666015
sergey-png <svsobolev@miem.hse.ru>
2022-03-26 17:46:56 +0300

    Start v.0.1.0

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import pyvisa as visa
 from time import sleep
 import numpy as np
-from pyvisa import Resource
 
 visa.log_to_screen()
 rm = visa.ResourceManager()
 # print(rm.list_resources())
-rigol = rm.list_resources()[0]
+rigol_device_id = rm.list_resources()[0]
 
 #  osc.read osc.write osc.query
 #  You can use visa-shell
@@ -14,14 +13,22 @@ rigol = rm.list_resources()[0]
 #  After opening device You can talk to the device using "write", "read" or "query".
 #  The default end of message is added to each message.
 #  osc.query - method 621 string in pyvisa/resources/messagebased.py
-osc = rm.open_resource(rigol)  # open device to work with
+osc = rm.open_resource(rigol_device_id)  # open device to work with
 print(f'QUERY returned answer = {osc.query("*IDN?")}')  # Command to check ID of opened device
 print(f"Opened res = {rm.list_opened_resources()}")
 
 
 def autoscale(oscillator):
+    """
+    Enable the waveform auto setting function. The oscilloscope will automatically adjust the
+    vertical scale, horizontal timebase, and trigger mode according to the input signal to
+    realize optimum waveform display. This command is equivalent to pressing the AUTO key
+    at the front panel.
+    :param oscillator: opened device to connect
+    """
     command = ":AUToscale"
     oscillator.write(command)
+    return 0
 
 
 print(f"Write to osc = {autoscale(osc)}")
