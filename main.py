4d64ebd41ae71852bf44e22b993cf1f182306c62
sergey-png <svsobolev@miem.hse.ru>
2022-04-05 18:52:27 +0300

    General Changes 05.04.2022

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import collections
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import multiprocessing as mp
+from threading import Thread, Lock
 import tqdm
 from rigol1000z import rigol1000z
 
@@ -26,9 +27,23 @@ class RigolAPI:
         if len(self.rm.list_resources()) > 0:
             self.rigol_device_id = self.rm.list_resources()[0]
             self.device = self.rm.open_resource(self.rigol_device_id)  # Now can self.device. -/read, -/write, -/query
-            self.device.timeout = 13000
+            self.device.timeout = 10000
         else:
             self.rigol_device_id = None
+            self.device = None
+
+    def reconnect(self):
+        self.device.write(":RUN")
+        self.device.close()
+        self.rm = visa.ResourceManager()
+        if len(self.rm.list_resources()) > 0:
+            self.rigol_device_id = self.rm.list_resources()[0]
+            self.device = self.rm.open_resource(self.rigol_device_id)
+            self.device.timeout = 10000
+            self.device.write(":RUN")
+        else:
+            self.rigol_device_id = None
+        return
 
     def device_id(self):
         return self.rigol_device_id
@@ -63,58 +78,138 @@ class RigolAPI:
         """
         :return: current // front phase of channel 1 and 2 in degrees
         """
+        self.device.write(":RUN")
         return float(self.device.query(":MEASure:ITEM? RPHase"))
 
-    def get_fphase(self):
-        """
-        :return: current \\ front phase of channel 1 and 2 in degrees
-        """
-        return float(self.device.query(":MEASure:ITEM? FPHase"))
+    def get_data(self, channel=1, mode="NORMal", form="ASCii"):
+        self.device.write(":RUN")
+        self.device.write(":STOP")
+        self.device.write(f":WAV:SOUR CHAN{channel}")
+        self.device.write(f":WAV:MODE {mode}")
+        self.device.write(f":WAV:FORM {form}")
+        message = self.device.query(":WAV:DATA?")
+        result = []
+        for element in message[11:].split(','):
+            result.append(float(element))
+        return result
 
 
-def draw_rphase(mute):
+# --------------- GLOBAL VARIABLES ---------------
+rigol = RigolAPI()
+data_channel = [[], []]
+phase_delay = collections.deque([0])
+
+
+# --------------- GLOBAL VARIABLES ---------------
+
+
+def get_data_thread(mute: Lock):
+    global data_channel
+    while True:
+        sleep(1)
+        print('I am thread for channels')
+        mute.acquire(blocking=True)
+        try:
+            data_channel[0] = rigol.get_data(1)
+            data_channel[1] = rigol.get_data(2)
+        except Exception as exp:
+            print(f"ERROR = {exp}")
+        finally:
+            sleep(0.1)
+            mute.release()
+
+
+def get_rphase_thread(mute: Lock):
+    global phase_delay
+    while True:
+        print(f'Phase delay = {phase_delay}')
+        sleep(0.1)
+        try:
+            mute.acquire(blocking=True)
+            data: float = rigol.get_rphase()
+            phase_delay.append(data)
+        except Exception as exp:
+            print(f"ERROR = {exp}")
+        finally:
+            sleep(0.1)
+            mute.release()
+
+
+def clearing_device(mute: Lock):
+    while True:
+        print("I am still here")
+        sleep(5)
+        mute.acquire(blocking=True)
+        sleep(0.1)
+        try:
+            if rigol.rigol_device_id is None:
+                continue
+            else:
+                print("RECONNECTED!")
+        except Exception as exp:
+            print(f"ОШИБКА ПОДКЛЮЧЕНИЯ = {exp}")
+            sleep(0.5)
+        finally:
+            mute.release()
+
+
+def draw_figures():
     # function to update the data
     def my_function(i):
+        global data_channel, phase_delay
         # get data
         rphase_data.popleft()
-        mute.acquire()
-        rphase_data.append(round(rigol.get_rphase(), 3))
-        mute.release()
+        rphase_data.append(phase_delay[0])
+        if len(phase_delay) > 1:
+            phase_delay.popleft()
+        y_axis_1 = data_channel[0]
+        y_axis_2 = data_channel[1]
         # clear axis
         ax.cla()
+        ax2.cla()
         # plot rphase_data
         ax.plot(rphase_data)
-        plt.title(label='Phase Delay')
+        plt.title(label='Phase Delay and Signals')
         plt.xlabel("Time (100ns / 1_read)")
         plt.ylabel("Phase Delay (deg)")
         ax.scatter(len(rphase_data) - 1, rphase_data[-1])
         ax.text(len(rphase_data) - 1, rphase_data[-1] + 2, "{}°".format(rphase_data[-1]))
         ax.set_ylim(-180, 180)
 
+        # plot signals from Channel 1 and 2
+        ax2.plot(y_axis_1, color='r', label='Channel 1')
+        ax2.plot(y_axis_2, color='b', label='Channel 2')
+        ax2.legend()
+        print("-------------- UPDATED GRAPH --------------")
+
     # start collections with zeros
     rphase_data = collections.deque(np.zeros(100))
 
     # define and adjust figure
-    fig = plt.figure(figsize=(7, 5), facecolor='#DEDEDE')
-    ax = plt.subplot(111)
+    fig = plt.figure(figsize=(10, 5), facecolor='#DEDEDE')
+    ax = plt.subplot(121)
+    ax2 = plt.subplot(122)
 
-    print(ax)
     ax.set_facecolor('#DEDEDE')
+    ax2.set_facecolor('#DEDEDE')
     # animate
-    ani = FuncAnimation(fig, my_function, interval=100)
+    ani = FuncAnimation(fig, my_function, interval=200)
     plt.show()
 
 
-rigol = RigolAPI()
-
 if __name__ == "__main__":
-    print(float(rigol.range(channel=1)))
-    print(float(rigol.range(channel=2)))
     print("Main Statistic")
     # Creating process
-    mutex = mp.Lock()
-    proc1 = mp.Process(target=draw_rphase, daemon=False, args=(mutex,))
-    proc1.start()
+    mutex = Lock()
+    getting_data = Thread(target=get_data_thread, args=(mutex,), daemon=True)
+    getting_phase = Thread(target=get_rphase_thread, args=(mutex,), daemon=True)
+    clearing = Thread(target=clearing_device, args=(mutex,), daemon=True)
+    clearing.start()
+    getting_phase.start()
+    getting_data.start()
+    sleep(1)
+
+    draw_figures()
 
     print("Created new process")
     print("------------END------------")
